7f9c26e5d87095b82587063272ddf42886b04bbc
Thom Allen <thomwork@gmail.com>
2017-10-09 17:04:55 +0100

    Point this repo should have started at (http.createServer)

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-var express = require('express');
-var app = express();
+var http = require('http');
 
-app.get('', function ( req, res ) {
-	res.send( 'Hello world' );
-});
+var server = http.createServer(function(request, response) {
+	response.write('hi');
+	response.end();
+})
 
-var server = app.listen( 3000, function() {
+server.listen(3333, function() {
 	var host = server.address().address;
 	var port = server.address().port;
-
-	console.log( 'Example app listening at http://%s:%s', host, port);
+	console.log( 'Example app listening at http://%s', host, port);
 });
-
